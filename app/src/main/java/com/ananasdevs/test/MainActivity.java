35fa351596e923a98537faaee2434933dac1b123
arian <arian.r.1373@gmail.com>
2016-07-21 22:03:34 +0430

    new

---
+++ app/src/main/java/com/ananasdevs/test/MainActivity.java
@@ -0,0 +1,84 @@
+package com.ananasdevs.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.ananasdevs.test.model.calendar.persian.CalendarTool;
+import com.ananasdevs.test.view.CalendarView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashSet;
+
+
+public class MainActivity extends Activity {
+
+    CalendarView cv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        HashSet<Date> events = new HashSet<>();
+        events.add(new Date());
+
+        cv = ((CalendarView)findViewById(R.id.calendar_view));
+        cv.setDateSystem(CalendarView.DateSystem.Persian);
+        //cv.updateCalendar(events);
+        cv.setSize(300, 300);
+        // assign event handler
+        cv.setEventHandler(new CalendarView.EventHandler() {
+            @Override
+            public void onDayLongPress(Date date) {
+                // show returned day
+                DateFormat df = SimpleDateFormat.getDateInstance();
+                Toast.makeText(MainActivity.this, df.format(date), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onDayPress(Date date) {
+                DateFormat df = SimpleDateFormat.getDateInstance();
+                Toast.makeText(MainActivity.this, df.format(date), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    public void Persian(View v) {
+        cv.setDateSystem(CalendarView.DateSystem.Persian);
+    }
+
+    public void Gregorian(View v) {
+        cv.setDateSystem(CalendarView.DateSystem.Gregorian);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
